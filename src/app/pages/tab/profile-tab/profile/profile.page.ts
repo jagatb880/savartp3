4f9c5013a1b8a93ae1711094eb1a3c82612b509d
debiprasad55100 <debiprasad55100@gmail.com>
2021-08-03 20:57:59 +0530

    all

--- src/app/pages/tab/profile-tab/profile/profile.page.ts
+++ src/app/pages/tab/profile-tab/profile/profile.page.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+  p_bar_value: number;
+  r_bar_value:number;
+  k_bar_value:number;
+  planreached : boolean;
+  planreachedome: boolean;
+  count: number;
+  constructor() {
+    this.planreached = true;
+    this.planreachedome = false;
+    this.count= 16;
+    this.setPercentBar()
+    this.firststausbar()
+    this.secondstausbar()
+   }
 
   ngOnInit() {
   }
+  setPercentBar() {
+    setTimeout(() => {
+      let apc = (this.count/100)
+      console.log(apc);
+      this.p_bar_value = apc;
+    }, );
+  }
 
+  firststausbar() {
+    setTimeout(() => {
+      let apc = (100 / 100)
+      console.log(apc);
+      this.r_bar_value = apc;
+    }, );
+  }
+  secondstausbar() {
+    setTimeout(() => {
+      let apc = (0 / 100)
+      console.log(apc);
+      this.k_bar_value = apc;
+    }, );
+  }
 }
