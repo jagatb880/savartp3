7b4ccbac5c3754c5084a0716347628558f06d122
debiprasad55100 <debiprasad55100@gmail.com>
2021-08-04 20:13:39 +0530

    all

--- src/app/pages/tab/portfolio-tab/goal-manager/goal-manager.page.ts
+++ src/app/pages/tab/portfolio-tab/goal-manager/goal-manager.page.ts
@@ -8,14 +8,43 @@ import { Location } from '@angular/common';
   styleUrls: ['./goal-manager.page.scss'],
 })
 export class GoalManagerPage implements OnInit {
+  p_bar_value: any;
+  datas: { targetamount: string; 'monthly_investmet_amount': string; 'goal_duartion': string; 'started_date': string; 'ended_date': string; progresscount: string; }[];
+  count: any;
   
   constructor(private location: Location){
+    // this.count = this.datas[0].progresscount.toString();
     
+    
+    this.datas = [{'targetamount':'10,000','monthly_investmet_amount':'10,000',
+    'goal_duartion':'June 2021 - June 2024','started_date':'21 Feb 2020','ended_date':'16 Aug 2021','progresscount':'16'},
+    {'targetamount':'10,000','monthly_investmet_amount':'10,000',
+    'goal_duartion':'June 2021 - June 2024','started_date':'21 Feb 2020','ended_date':'16 Aug 2021','progresscount':'90'},
+    {'targetamount':'10,000','monthly_investmet_amount':'10,000',
+    'goal_duartion':'June 2021 - June 2024','started_date':'21 Feb 2020','ended_date':'16 Aug 2021','progresscount':'70'}]
+    for(let i = 0;i<this.datas.length;i++){
+      if(this.datas[i]){
+        this.count = this.datas[i].progresscount;
+        console.log(this.count);
+        setTimeout(() => {
+          let apc = (this.count/100)
+          console.log(apc);
+          this.p_bar_value = apc;
+        }, );
+        // this.setPercentBar(this.count);
+      }
+    }
   }
 
   ngOnInit() {
   }
-  
+  // setPercentBar(i) {
+  //   setTimeout(() => {
+  //     let apc = (i/100)
+  //     console.log(apc);
+  //     this.p_bar_value = apc;
+  //   }, );
+  // }
   
   backBtn(){
     this.location.back();
